4e519e2c3e26e00d344b86b3e8314f20c6294476
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-02-14 14:18:08 -0300

    Update pong - Copia.py

--- pong - Copia.py
+++ pong - Copia.py
@@ -6,7 +6,7 @@
 
 import turtle
 import os
-# som de colisão
+# efeito de colisão
 import pygame
 pygame.init()
 pygame.mixer.music.load('collision.mp3')
@@ -18,11 +18,27 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
+# Mesa de tenis
+table = turtle.Turtle()
+table.shape("square")
+table.color('green')
+table.shapesize(stretch_wid=22, stretch_len=37)
+table.penup()
+table.goto(0, 0)
+
+# Linha da mesa
+line = turtle.Turtle()
+line.shape("square")
+line.color('white')
+line.shapesize(stretch_wid=22, stretch_len=0.5)
+line.penup()
+line.goto(0, 0)
+
 # desenhar raquete 1
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
-paddle_1.color("white")
+paddle_1.color("red")
 paddle_1.shapesize(stretch_wid=5, stretch_len=1)
 paddle_1.penup()
 paddle_1.goto(-350, 0)
@@ -31,7 +47,7 @@ paddle_1.goto(-350, 0)
 paddle_2 = turtle.Turtle()
 paddle_2.speed(0)
 paddle_2.shape("square")
-paddle_2.color("white")
+paddle_2.color("blue")
 paddle_2.shapesize(stretch_wid=5, stretch_len=1)
 paddle_2.penup()
 paddle_2.goto(350, 0)
